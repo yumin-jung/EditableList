cd24a089d64b325c3b13f90f04b43353b2940868
yumin-jung <jungym887@naver.com>
2025-07-24 13:39:27 +0900

    Initial Commit

---
+++ EditableList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EditableList
+//
+//  Created by yumin on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EditableList/EditableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditableListApp.swift
+//  EditableList
+//
+//  Created by yumin on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EditableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
